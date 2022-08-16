d86d07bdb51eac17d9052fd79997fe21612e0e94
Jonathan-Moreira <108627997+Jonathan-Moreira@users.noreply.github.com>
2022-08-16 18:36:53 -0300

    exercicio-laços

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let verificar = prompt ('Você deseja comer mais coxinhas? se sim, digite S! se não, digite N').toUpperCase
+let conta = 2.50
+
+while(verificar !== 'S'){
+    alert('Não quer mais coxinhas')
+    verificar = prompt('Você deseja comer mais coxinhas? se sim, digite S! se não, digite N').toUpperCase
+    
+    alert('quer mais uma coxinha')
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,6 @@
+const arrayString = ['Rúsia', 'Canadá', 'China', 'EUA', 'Brasil']
+
+for(let i = 0; i < arrayString.length; i++){
+    console.log(`"ranking ${i} -> ${arrayString[i]}"`)
+}
+
